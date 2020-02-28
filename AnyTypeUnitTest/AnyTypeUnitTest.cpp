bde4f01204d11a7b7dfad963544164dd71ac4a91
Oleh Dmytrash <dmutrash61@gmail.com>
2020-02-28 16:37:39 +0200

    Added more test cases

--- AnyTypeUnitTest/AnyTypeUnitTest.cpp
+++ AnyTypeUnitTest/AnyTypeUnitTest.cpp
@@ -32,6 +32,25 @@ namespace AnyTypeUnitTest
 			Assert::AreEqual(anyType1.GetType(), std::string("int"), L"Should be int");
 		}
 
+		TEST_METHOD(InitTest4)
+		{
+			AnyType anyType;
+			anyType = '\0';
+			Assert::AreEqual(anyType.GetType(), std::string("char"), L"Should be int");
+		}
+
+		TEST_METHOD(InitTest5)
+		{
+			AnyType anyType = 5.6254;			
+			Assert::AreEqual(anyType.GetType(), std::string("double"), L"Should be int");
+		}
+
+		TEST_METHOD(InitTest6)
+		{
+			AnyType anyType = false;
+			Assert::AreEqual(anyType.ToBool(), false, L"Should be int");
+		}
+
 		TEST_METHOD(GetTypeTest1)
 		{
 			AnyType anyType = 5;			
@@ -41,7 +60,99 @@ namespace AnyTypeUnitTest
 		TEST_METHOD(GetTypeTest2)
 		{
 			AnyType anyType = 5;
-			Assert::AreEqual(anyType.GetType(), std::string("bool"), L"Should be int");
+			Assert::AreEqual(anyType.GetType(), std::string("bool"), L"Negative test");
+		}
+
+		TEST_METHOD(SafeCastTest1)
+		{
+			AnyType anyType = 5;
+			bool exception_is_generated = false;
+			try
+			{
+				double val_lf = anyType.ToDouble();
+			}
+			catch (const std::exception& ex)
+			{
+				exception_is_generated = true;
+			}			
+			Assert::IsFalse(exception_is_generated, L"Should be true");
+		}
+
+		TEST_METHOD(SafeCastTest2)
+		{
+			AnyType anyType = 5;
+			bool exception_is_generated = false;
+			try
+			{
+				bool val_lf = anyType.ToBool();
+			}
+			catch (const std::exception & ex)
+			{
+				exception_is_generated = true;
+			}
+			Assert::IsTrue(exception_is_generated, L"Should be true");
+		}
+
+		TEST_METHOD(SafeCastTest3)
+		{
+			AnyType anyType = 5.5;
+			bool exception_is_generated = false;
+			try
+			{
+				double val_lf = anyType.ToBool();
+			}
+			catch (const std::exception & ex)
+			{
+				exception_is_generated = true;
+			}
+			Assert::IsFalse(exception_is_generated, L"Negative test");
+		}
+
+		TEST_METHOD(SafeCastTest4)
+		{
+			AnyType anyType = true;
+			bool exception_is_generated = false;
+			try
+			{
+				bool val_lf = anyType.ToBool();
+			}
+			catch (const std::exception & ex)
+			{
+				exception_is_generated = true;
+			}
+			Assert::IsFalse(exception_is_generated, L"Should be true");
+		}
+
+		TEST_METHOD(SafeCastTest5)
+		{
+			AnyType anyType = true;
+			bool exception_is_generated = false;
+			try
+			{
+				bool val_lf = anyType.ToInt();
+			}
+			catch (const std::exception & ex)
+			{
+				exception_is_generated = true;
+			}
+			Assert::IsFalse(exception_is_generated, L"Should be int");
+		}
+
+		TEST_METHOD(SwapTest1)
+		{
+			AnyType anyType(5);
+			AnyType anyType1(true);
+			anyType.swap(anyType1);
+			Assert::IsTrue((anyType1.ToInt() == 5 && anyType.ToBool() == true), L"Should be true");
+		}
+
+		TEST_METHOD(SwapTest2)
+		{
+			AnyType anyType(5);
+			AnyType anyType1(true);
+			anyType.swap(anyType1);
+			//must be exeption
+			Assert::IsTrue((anyType1.ToInt() == 5 && anyType1.ToBool() == true), L"Negative test");
 		}
 	};
 }
